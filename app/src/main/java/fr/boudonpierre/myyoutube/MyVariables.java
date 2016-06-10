571fd650082fc95a669fbb3398cc36603d3e7c5c
Openfield Mobility <pboudon@opnf.fr>
2016-06-10 11:26:40 +0200

    Code refactor in MyVariables

--- app/src/main/java/fr/boudonpierre/myyoutube/MyVariables.java
+++ app/src/main/java/fr/boudonpierre/myyoutube/MyVariables.java
@@ -14,18 +14,27 @@ import java.util.ArrayList;
  * Created by OpenFieldMacMini on 08/06/2016.
  */
 public class MyVariables {
+    /* TAG */
     public static final String SPTAG = "starredVideo";
+
+
+    /* USERS INFORMATION */
     public static final String[] usernames = {"Dexter MORGAN", "Odile DERAY", "Brenda MONTGOMERY", "Knights Who Say NI !"};
     public static final String[] emails = {"dexmorgan@ClickOn.me", "oderay@ClickOn.me", "bbbbrendaaa@ClickOn.me", "shrubbery@ClickOn.me"};
     public static final int[] profileImages = {R.drawable.t_profile, R.drawable.oderay, R.drawable.bmontgomery, R.drawable.ni};
     public static final int[] backgroundImages = {R.drawable.t_background_poly, R.drawable.background1, R.drawable.background2, R.drawable.background3};
 
+
+    /* DATA */
     public static ArrayList<Video> videos;
     public static ArrayList<Video> starredVideos;
     public static Video currentVideo;
     public static int currentUser;
 
+
+    /* SAVE & RETRIEVE METHODS */
     public static void saveStarredVideos(Context context) {
+        // Save array of starred videos
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
         SharedPreferences.Editor editor = sharedPrefs.edit();
         Gson gson = new Gson();
@@ -37,18 +46,21 @@ public class MyVariables {
     }
 
     public static void retrieveStarredVideos(Context context) {
+        // Retrieve array of starred videos
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
         Gson gson = new Gson();
         String json = sharedPrefs.getString(MyVariables.SPTAG, null);
         Type type = new TypeToken<ArrayList<Video>>() {}.getType();
         MyVariables.starredVideos = gson.fromJson(json, type);
 
+        // Be sure to not have a null array
         if (MyVariables.starredVideos == null) {
             MyVariables.starredVideos = new ArrayList<Video>();
         }
     }
 
     public static void saveCurrentUser(Context context) {
+        // Save current user in header
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
         SharedPreferences.Editor editor = sharedPrefs.edit();
 
@@ -57,6 +69,7 @@ public class MyVariables {
     }
 
     public static void retrieveCurrentUser(Context context) {
+        // Retrieve current user in header
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
         MyVariables.currentUser = sharedPrefs.getInt("currentUser", 0);
     }
